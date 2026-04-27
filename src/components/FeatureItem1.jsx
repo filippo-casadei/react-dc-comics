223a061603d6c2f3e870fc98195fd3dc50387076
Filippo Casadei <casadeifilippo91@gmail.com>
2026-04-27 18:30:28 +0200

    -Eliminate due img inutilizzate dalla cartella public. -Creato il component FeaturesBar ed esportato. -Importato il component FeaturesBar in App.jsx. -Creato il component FeatureItem1.

---
+++ src/components/FeatureItem1.jsx
@@ -0,0 +1,10 @@
+function FeatureItem1() {
+    return (
+        <div className="feature-item">
+            <img src="/buy-comics-digital-comics.png"></img>
+            <p>Digital Comics</p>
+        </div>
+    )
+};
+
+export default FeatureItem1;
---
+++ src/components/FeaturesBar.jsx
@@ -0,0 +1,15 @@
+import FeatureItem1 from "./FeatureItem1.jsx";
+
+function FeaturesBar () {
+    return (
+        <section className="feature-bar">
+            <FeatureItem1 />
+            <FeatureItem2 />
+            <FeatureItem3 />
+            <FeatureItem4 />
+            <FeatureItem5 />
+        </section>
+    )
+};
+
+export default FeaturesBar;
