6a4eaedae835934dffe9391081ba445effc5d5d1
Filippo Casadei <casadeifilippo91@gmail.com>
2026-04-27 18:40:57 +0200

    -Importata FeaturesBar in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from './components/Header.jsx';
 import Hero from './components/Hero.jsx';
+import FeaturesBar from './components/FeaturesBar.jsx';
 import './App.css'
 
 function App() {
   return <>
     <Header />
     <Hero />
+    <FeaturesBar />
   </>
 };
 
