1619c21800cd0bbcd55f751b5fdb538205c5da08
Filippo Casadei <casadeifilippo91@gmail.com>
2026-04-27 17:26:59 +0200

    Modificato il file App.jsx: -Eliminati gli import inutili, importato il componente Header per prova.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import Header from './components/Header.jsx';
 import './App.css'
 
 function App() {
-  
+  return <>
+    <Header />
+  </>
 };
 
 export default App
