f3f22d9ad67e41bbbdac9b907578b3ac40149904
Filippo Casadei <casadeifilippo91@gmail.com>
2026-04-28 00:26:02 +0200

    -Importato Bootstrap -Modificato app.css

--- src/components/Footer1.jsx
+++ src/components/Footer1.jsx
@@ -1,53 +1,54 @@
 function Footer1 () {
     return (
-        <div className="contenitore-footer1">
-            <div>
-                <h4>DC Comics</h4>
-                <ul>
-                    <li>Characters</li>
-                    <li>Comics</li>
-                    <li>Movies</li>
-                    <li>TV</li>
-                    <li>Games</li>
-                    <li>Videos</li>
-                    <li>News</li>
-                </ul>
-            </div>
+        <div className="contenitore-footer1 container-fluid text-white py-5">
+            <div className="row">
+                <div className="col-md-3">
+                    <h4>DC Comics</h4>
+                    <ul className="list-unstyled">
+                        <li>Characters</li>
+                        <li>Comics</li>
+                        <li>Movies</li>
+                        <li>TV</li>
+                        <li>Games</li>
+                        <li>Videos</li>
+                        <li>News</li>
+                    </ul>
+                
+                
+                    <h4 className="mt-4">Shop</h4>
+                    <ul className="list-unstyled">
+                        <li>Shop DC</li>
+                        <li>Shop DC Collectibles</li>
+                    </ul>
+                </div>
 
-            <div>
-                <h4>Shop</h4>
-                <ul>
-                    <li>Shop DC</li>
-                    <li>Shop DC Collectibles</li>
-                </ul>
-            </div>
+                <div className="col-md-3">
+                    <h4>DC</h4>
+                    <ul className="list-unstyled">
+                        <li>Terms of Use</li>
+                        <li>Privacy policy New</li>
+                        <li>Ad Choices</li>
+                        <li>Advertising</li>
+                        <li>Jobs</li>
+                        <li>Subscriptions</li>
+                        <li>Talent Workshops</li>
+                        <li>CPSC Certificates</li>
+                        <li>Ratings</li>
+                        <li>Shop Help</li>
+                        <li>Contact Us</li>
+                    </ul>
+                </div>
 
-            <div>
-                <h4>DC</h4>
-                <ul>
-                    <li>Terms of Use</li>
-                    <li>Privacy policy New</li>
-                    <li>Ad Choices</li>
-                    <li>Advertising</li>
-                    <li>Jobs</li>
-                    <li>Subscriptions</li>
-                    <li>Talent Workshops</li>
-                    <li>CPSC Certificates</li>
-                    <li>Ratings</li>
-                    <li>Shop Help</li>
-                    <li>Contact Us</li>
-                </ul>
-            </div>
-
-            <div>
-                <h4>Sites</h4>
-                <ul>
-                    <li>DC</li>
-                    <li>MAD Magazine</li>
-                    <li>DC Kids</li>
-                    <li>DC Universe</li>
-                    <li>DC Power Visa</li>
-                </ul>
+                <div  className="col-md-3">
+                    <h4>Sites</h4>
+                    <ul className="list-unstyled">
+                        <li>DC</li>
+                        <li>MAD Magazine</li>
+                        <li>DC Kids</li>
+                        <li>DC Universe</li>
+                        <li>DC Power Visa</li>
+                    </ul>
+                </div>
             </div>
         </div>
 
