0323ef562629c53ad7517d939caa7f78c84cf764
Filippo Casadei <casadeifilippo91@gmail.com>
2026-04-27 19:04:47 +0200

    -Creata prima parte del Footer, "Footer1" ed esportata. -Importato "Footer1" in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import Header from './components/Header.jsx';
 import Hero from './components/Hero.jsx';
 import FeaturesBar from './components/FeaturesBar.jsx';
+import Footer1 from './components/Footer1.jsx';
 import './App.css'
+
 
 function App() {
   return <>
     <Header />
     <Hero />
     <FeaturesBar />
+    <Footer1 />
   </>
 };
 
---
+++ src/components/Footer1.jsx
@@ -0,0 +1,57 @@
+function Footer1 () {
+    return (
+        <div className="footer1-list">
+            <div>
+                <h4>DC Comics</h4>
+                <ul>
+                    <li>Characters</li>
+                    <li>Comics</li>
+                    <li>Movies</li>
+                    <li>TV</li>
+                    <li>Games</li>
+                    <li>Videos</li>
+                    <li>News</li>
+                </ul>
+            </div>
+
+            <div>
+                <h4>Shop</h4>
+                <ul>
+                    <li>Shop DC</li>
+                    <li>Shop DC Collectibles</li>
+                </ul>
+            </div>
+
+            <div>
+                <h4>DC</h4>
+                <ul>
+                    <li>Terms of Use</li>
+                    <li>Privacy policy New</li>
+                    <li>Ad Choices</li>
+                    <li>Advertising</li>
+                    <li>Jobs</li>
+                    <li>Subscriptions</li>
+                    <li>Talent Workshops</li>
+                    <li>CPSC Certificates</li>
+                    <li>Ratings</li>
+                    <li>Shop Help</li>
+                    <li>Contact Us</li>
+                </ul>
+            </div>
+
+            <div>
+                <h4>Sites</h4>
+                <ul>
+                    <li>DC</li>
+                    <li>MAD Magazine</li>
+                    <li>DC Kids</li>
+                    <li>DC Universe</li>
+                    <li>DC Power Visa</li>
+                </ul>
+            </div>
+        </div>
+
+    )
+};
+
+export default Footer1;
