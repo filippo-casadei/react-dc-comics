b72aee36d10d158ca2a24dd4e81767f5556089ab
Filippo Casadei <casadeifilippo91@gmail.com>
2026-04-27 18:03:30 +0200

    -Creato component "Hero" nella cartella components ed esportato. -Importato component "Hero" in App.jsx e aggiunto nella struttura.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from './components/Header.jsx';
+import Hero from './components/Hero.jsx';
 import './App.css'
 
 function App() {
   return <>
     <Header />
+    <Hero />
   </>
 };
 
---
+++ src/components/Hero.jsx
@@ -0,0 +1,9 @@
+function Hero() {
+    return (
+        <section className="contenuto-hero">
+            <p>--Content Goes Here--</p>
+        </section>
+    )
+};
+
+export default Hero;
