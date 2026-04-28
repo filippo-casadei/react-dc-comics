eeae10d6779e32627053f63ee164179b01de667e
Filippo Casadei <casadeifilippo91@gmail.com>
2026-04-28 17:25:52 +0200

    -Creato componente prototipo per la sezione con tutti i fumetti.

---
+++ src/components/ComixBar.jsx
@@ -0,0 +1,14 @@
+import comics from "../data/comics.js";
+
+function ComixBar() {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-2">
+                    <img src=""></img>
+                    <p></p>
+                </div>
+            </div>
+        </div>
+    )
+};
