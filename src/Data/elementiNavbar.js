45d4c5ab3a70cb261116b73ca16e41eea0cfcf3e
Filippo Casadei <casadeifilippo91@gmail.com>
2026-04-28 18:55:39 +0200

    -Creato nuovo file elementiNavbar.js con array oggetti per ricreare la navBar con .map

---
+++ src/Data/elementiNavbar.js
@@ -0,0 +1,45 @@
+
+const elementiNavbar = [
+    {
+        id: 1,
+        title: "Characters"
+    },
+    {
+        id: 2,
+        title: "Comics"
+    },
+    {
+        id: 3,
+        title: "Movies"
+    },
+    {
+        id: 4,
+        title: "TV"
+    },
+    {
+        id: 5,
+        title: "Games"
+    },
+    {
+        id: 6,
+        title: "Collectibles"
+    },
+    {
+        id: 7,
+        title: "Videos"
+    },
+    {
+        id: 8,
+        title: "Fans"
+    },
+    {
+        id: 9,
+        title: "News"
+    },
+    {
+        id: 10,
+        title: "Shop"
+    }
+];
+
+export default elementiNavbar;
