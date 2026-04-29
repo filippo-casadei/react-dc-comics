898719f6cd2f5b263dccde0a9fb1b3dadc6e8fa6
Filippo Casadei <casadeifilippo91@gmail.com>
2026-04-29 19:15:33 +0200

    -Modificato il return del nuovo componente ComicCard, seguendo lo style del precedente.

--- src/components/ComicCard.jsx
+++ src/components/ComicCard.jsx
@@ -1,5 +1,12 @@
+import styles from "./ComixBar.module.css"
+
 function ComicCard(props) {
-    return <h1>{props.comic.series}</h1>;
+    return (
+        <div className="text-white text-center">
+            <img className={`${styles.imgStyle} mb-2`} src={props.comic.thumb}></img>
+            <p>{props.comic.series}</p>
+        </div>
+    )
 };
 
 export default ComicCard;
